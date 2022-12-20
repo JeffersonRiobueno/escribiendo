a8749e4c0be62a57e7ef85e395250df0187a4732
Jefferson Riobueno <riobueno.jr@gmail.com>
2022-12-19 23:06:01 -0500

    blog

--- single.php
+++ single.php
@@ -1,31 +1,45 @@
 <!-- Archivo de cabecera gobal de Single -->
 <?php get_header(); ?>
+
+<!-- Contenido del post -->
 <?php if ( have_posts() ) : the_post(); ?>
 <?php  //the_post_thumbnail(0); 
-        //$thumbID = get_post_thumbnail_id( $post->ID );
-        //$imgDestacada = wp_get_attachment_image_src( $thumbID, 'large' ); // Sustituir por thumbnail, medium, large o full
+        $thumbID = get_post_thumbnail_id( $post->ID );
+        $imgDestacada = wp_get_attachment_image_src( $thumbID, 'large' ); // Sustituir por thumbnail, medium, large o full
           // dentro del loop no es necesario pasar el $post ID
           //$custom_fields = get_post_custom();
           $category = get_the_category();
         ?>
-      <section class="wrap">
-        <div class="content">
-          <div class="bloque_articulo">
-            <div class="post">
-              <h1><?php the_title(); ?></h1>
-              <div class="fila">
-                  <span><?php the_time('j F, Y'); ?></span>
-                  <span class="cat"> | <?php echo $category[0]->cat_name; ?></span>
-              </div>
-              <?php the_content(); ?>
-            </div>
-          </div>
-        </div>
-<!--        <div class="sidebar">
-          <?php include 'template-sidebar.php'; ?>
-        </div>-->
-      </section>
+      
+<section class="entrada">
 
+  <section class="post">
+      <figure style="background-image: url('<?php echo "$imgDestacada[0]"; ?>');">
+        <header>
+          <h1><?php the_title(); ?></h1>
+        </header>
+      </figure>
+
+      <div class="cont-post">
+        <div class="datos">
+          <div class="cat"><?php echo $category[0]->cat_name; ?></div> 
+          <div class="fecha"><?php the_time('j F, Y'); ?></div>
+        </div>
+
+         <?php the_content(); ?>
+
+        </div>
+      </div>
+      
+      <!-- Comentarios -->
+      <?php //comments_template(); ?>
+      <section class="sidebar">
+
+          <?php dynamic_sidebar('Barra'); ?>
+      </section>
+  </section>
+
+</section>
 
 <?php else : ?>
   <p><?php _e('Ups!, esta entrada no existe.'); ?></p>
